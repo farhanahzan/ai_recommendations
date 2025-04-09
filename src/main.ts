322678271db83f5bae19fca35635949216bc9187
farhanahzan <farhan98ahzan@gmail.com>
2025-04-09 10:27:07 +0530

    enable CORS for frontend domain and specified methods

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://officestationery.lk', // ✅ Allow your frontend domain
+    methods: ['GET', 'POST'],
+    credentials: true, // Optional: if you're using cookies
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
