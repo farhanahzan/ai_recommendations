7b4f40cab551354371986535402885a062b0a463
farhanahzan <farhan98ahzan@gmail.com>
2025-04-09 10:30:49 +0530

    enable CORS for specific frontend domain and methods

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,11 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: 'https://officestationery.lk',
+        methods: ['GET', 'POST'],
+        credentials: true,
+    });
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
